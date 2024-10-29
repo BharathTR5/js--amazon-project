b3d92ae1aee57331374e900681c91a3834d28f1c
BharathTR <bharathtr70260@gmail.com>
2024-10-29 11:33:30 +0530

    make the cartQ

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,7 +81,17 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         quantity: 1
     });
 
+
+    let cartQuantity=0;
+
+    cart.forEach((item) =>{
+      cartQuantity += item.quantity;
+      console.log(cartQuantity);
+    })
+
     console.log(cart);
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   }
 })
   });
